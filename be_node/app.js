4e11bf043b5b711f4eea1a34735f1f8d23adfe57
smintukas <smintukas@gmail.com>
2020-04-21 15:39:44 +0300

    Asynchronosc

--- be_node/app.js
+++ be_node/app.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3500
 
+const cacheHours = 24 * 60 * 60; //24 hours
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Max-Age', `${cacheHours}`);
+    next();
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+
 class Department {
     constructor(id, name, regionId) {
         this.id = id;
@@ -10,8 +23,9 @@ class Department {
     }
 }
 
-const greenHill = new Department(34, "Subaciaus 85", 'abd124')
+app.get('/all', (req, res) => {
 
-app.get('/', (req, res) => res.json(greenHill))
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+    const obj1 = new Department(34, "Subaciaus 83", 'abd124')
+    const obj2 = new Department(31, "Subaciaus 85", 'abd125')
+    res.json([obj1, obj2]);
+})
--- front_end/src/pages/department-page.js
+++ front_end/src/pages/department-page.js
@@ -1,11 +1,56 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
 
 const DepartmentPage = () => {
+    const [departments, setDepartments] = useState([])
+
+    function callBackEnd() {
+        fetch('http://localhost:3500/all')
+            .then(resp => {
+                 resp.json().then(json => {
+                    console.log(json)
+                    setDepartments(json);
+                });
+            })
+    }
+    const callBackEnd2 = async () => {
+        try {
+            const respBe = await fetch('http://localhost:3500/all')
+            console.log(1,respBe)
+            const respStream = await respBe.json()
+            console.log(2, respStream)
+            setDepartments(respStream)
+        } catch (e) {
+            console.log('error :D', e)
+        }
+    }
 
     return (
         <main className="department-page">
             <h1>Smints front end app</h1>
+            <h2>Departments</h2>
+            <pre>read abot ES6 (mozilla Mdn)</pre>
+            <button onClick={callBackEnd}>load departments</button>
+            <button onClick={callBackEnd2}>load departments 2</button>
+            <table>
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>region id</th>
+                </tr>
+                </thead>
+                <tbody>
+                {departments.map((obj, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{obj.id}</td>
+                            <td>{obj.name}</td>
+                            <td>{obj.regionId}</td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
         </main>
     )
 }
